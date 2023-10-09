7c4f4a1b077955e2a130ac96d67bd4f0b335bdde
Xeloses <37206095+Xeloses@users.noreply.github.com>
2023-10-09 22:07:22 +0300

    rename helpers to helper-*.js

---
+++ src/helpers/helper-export.js
@@ -0,0 +1,38 @@
+/**
+ * Exporting helpers.
+ * @module xeljslib/helpers/helpers-export
+ */
+
+/**
+ * @typedef  {Object} ExportingList
+ * @property {*} exporting_unit_name - Name of the exporting unit.
+ */
+
+/**
+ * Exports source units (classes/functions/consts/...) to the browser.
+ * Source units will be available in global scope as children of
+ * {window} and/or {unsafeWindow} (for userscripts).
+ *
+ * @param {ExportingList} exporting  List ({@link ExportingList}) of exporting units
+ *                                   (as pairs: **{ {string} Name: {any} Unit }**).
+ *                                   Source units will be exported with given names
+ *                                   *(all symbols in names except alphanumeric and
+ *                                   underscore will be converted to underscores)*.
+ * @return {void}
+ */
+function exportToBrowser(exporting)
+{
+    for(const key in exporting)
+    {
+        const name = key.replace(/[^\w]+/g, '_').replace(/_+/g, '_');
+
+        if(typeof unsafeWindow !== 'undefined'){ unsafeWindow[ name ] = exporting[ key ]; }
+        if(typeof window !== 'undefined'){ window[ name ] = exporting[ key ]; }
+    }
+}
+
+const ExportHelpers = {
+    toBrowser: exportToBrowser
+}
+
+export default ExportHelpers;
